0633265ac25683bf85537ff3b90f33d2be492a88
QUESADA MOLINA <alejandro.quesada11@ulatina.net>
2023-12-13 21:52:18 -0600

    Documentacion usuarios.

--- Login/Autenticacion.cs
+++ Login/Autenticacion.cs
@@ -4,12 +4,16 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace ProyectoPrograCuatro.Login
 {
+    // Filtro de acción para verificar la autenticación del usuario
     public class Autenticacion : ActionFilterAttribute
     {
+        // Método que se ejecuta antes de que se ejecute una acción del controlador
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
+            // Verifica si el usuario ha iniciado sesión (utilizando, por ejemplo, una variable de sesión "Usuario")
             if (filterContext.HttpContext.Session.GetString("Usuario") == null)
             {
+                // Si el usuario no ha iniciado sesión, redirige a la acción "Login" del controlador "Usuarios"
                 filterContext.Result = new RedirectToRouteResult(
                 new RouteValueDictionary {
                                 { "Controller", "Usuarios" },
